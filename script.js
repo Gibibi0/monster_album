9424f354d0dc413c59efdf9fd0e86a16e9bb0256
Gibibi0 <anast365antonova@gmail.com>
2025-06-03 13:11:18 +0300

    add link to jquery

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  $("#album").turn({
+    width: 800,
+    height: 500,
+    autoCenter: true,
+    gradients: true,
+    elevation: 50
+  })
+})
